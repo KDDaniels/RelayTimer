d1a73d9c292965b753635a9b3f1aea9d9d8676b7
AgehWewt <kendall.daniels.dev@gmail.com>
2025-05-28 22:04:57 -0500

    updated main a bit

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,23 @@
 #define FUNC_BTN 3
 #define TOGGLE_BTN 4
 
-/* ==== Time Vars ==== */
-unsigned long currentMillis = 0;
-
 void setup()
 {
   SCREEN::init();
   RELAY::init(RELAY_PIN);
+  TIMER::setInterval(5000);
 }
 
 void loop()
 {
-  currentMillis = millis();
-  SCREEN::setCursor(0, 1);
-  SCREEN::print(int(currentMillis/1000));
+  TIMER::update();
+  
+  if (TIMER::hasIntervalPassed())
+  {
+    RELAY::toggle();
+    TIMER::setTimerTarget(5000);
+  }
 
-  RELAY::toggle();
-
-  SCREEN::setCursor(0, 0);
-  SCREEN::print((RELAY::getState()) ? "enabled  " : "disabled  ");
-
-  delay(1000);
+  SCREEN::print("Time remaining: ", 0, 0);
+  SCREEN::print(TIMER::getTimeRemainingFormatted(), 0, 1);
 }
