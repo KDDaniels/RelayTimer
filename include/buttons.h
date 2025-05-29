135213c96f7dcfab6a2575025718ff6c1ac7aa0d
AgehWewt <kendall.daniels.dev@gmail.com>
2025-05-29 13:01:53 -0500

    updated button functionality

--- include/buttons.h
+++ include/buttons.h
@@ -1,9 +1,11 @@
 #ifndef BUTTONS_H
 #define BUTTONS_H
 
-namespace BUTTON
+namespace BUTTONS
 {
-
+    void init(int funcPin, int togglePin);
+    bool isfPinPressed();
+    bool istPinPressed();
 }
 
 #endif
--- src/buttons.cpp
+++ src/buttons.cpp
@@ -1,7 +1,32 @@
 #include "buttons.h"
 #include <Arduino.h>
 
-namespace BUTTON
+namespace
 {
-    
+    int fPin;
+    int tPin;
+    bool fPinPressed = false;
+    bool tPinPressed = false;
+}
+
+namespace BUTTONS
+{
+    void init(int funcPin, int togglePin)
+    {
+        fPin = funcPin;
+        tPin = togglePin;
+
+        pinMode(fPin, INPUT_PULLUP);
+        pinMode(tPin, INPUT_PULLUP);
+    }
+
+    bool isfPinPressed()
+    {
+        return (digitalRead(fPin) == LOW);
+    }
+
+    bool istPinPressed()
+    {
+        return (digitalRead(tPin) == LOW);
+    }
 }
